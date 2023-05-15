6a0047fd093074758dc82061c6c9b5ee3c58d98b
archemich <archemich@gmail.com>
2023-05-16 00:57:35 +0300

    bot simply send request and doesn't fail

--- production/swaprum-claim-bot/swaprum_claim_bot/main.py
+++ production/swaprum-claim-bot/swaprum_claim_bot/main.py
@@ -1,24 +1,46 @@
 import argparse
+import csv
+import logging
+import traceback
+from pathlib import Path
+from typing import List
+
+import requests
+
+def parse_csv(csv_p: Path) -> List[str]:
+    """
+    Parse csv and get list of wallet addresses.
+
+    Addresses expected to be in the second column.
+    """
+    with csv_p.open('r') as f:
+        reader = csv.reader(f, delimiter=';', lineterminator=';;;')
+        wallets = [row[1] for row in reader]
+        return wallets
 
 
+def log_exception(e: Exception):
+    traceback.print_exception(e)
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--email', required=True)
-    parser.add_argument('--user', required=True)
-    parser.add_argument('--pass', required=True)
-    parser.add_argument('--folder', required=True)
-    parser.add_argument('--phone', required=True)
-
+    parser.add_argument('--csv-wallets', type=Path, required=True,
+                        help='.csv file where stored wallet addresses '
+                             'in the second column')
     return parser.parse_args()
-def test_work(d):
-    s = vars(d)
-    for k in s:
-        print(f'{k} == {s[k]}')
 
 
 def main():
     args = parse_args()
-    test_work(args)
+    wallets = parse_csv(args.csv_wallets)
+    while True:
+        for wallet in wallets:
+            url = f'https://swaprum.finance/server/claim-free?address={wallet}'
+            print(f'GET request to {url}')
+            try:
+                res = requests.get(f'https://swaprum.finance/server/claim-free?address={wallet}')
+                print(f'Response: {res.text}')
+            except Exception as e:
+                log_exception(e)
 
 
 if __name__ == '__main__':
